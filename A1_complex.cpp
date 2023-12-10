238735e27cedb4e12d06e285be78bef6716ea5b8
Vedant024 <153173958+Vedant024@users.noreply.github.com>
2023-12-10 15:41:30 +0530

    A1_complex.cpp

--- A1_complex.cpp
+++ A1_complex.cpp
@@ -1,27 +1,18 @@
 
-/* 
-Implement a class Complex which represents the Complex Number data type. Implement the following
-1.	Constructor (including a default constructor which creates the complex number 0+0i).
-2.	Overload operator+ to add two complex numbers.
-3.	Overload operator* to multiply two complex numbers.
-4.	Overload operators << and >> to print and read Complex Numbers
-*/
-
-
 # include<iostream>
 using namespace std;
-class Complex                       //decaring Class Complex
+class Complex                       
  {
  double real;       
  double img;
  public: 
- Complex();            // Default Constructor
- friend istream & operator >> (istream &, Complex &); // Input 
- friend ostream & operator << (ostream &, const Complex &); // Output
- Complex operator + (Complex); // Addition
- Complex operator * (Complex); // Multiplication
+ Complex();           
+ friend istream & operator >> (istream &, Complex &);  
+ friend ostream & operator << (ostream &, const Complex &); 
+ Complex operator + (Complex); 
+ Complex operator * (Complex); 
  };
- Complex::Complex()                 // Default Constructor 
+ Complex::Complex()                  
  {
  real = 0;
  img = 0;
@@ -36,14 +27,14 @@ class Complex                       //decaring Class Complex
  cout << d.real << " + " << d.img << "i" << endl;
  return cout;
  }
- Complex Complex::operator + (Complex c1)   // Overloading + operator
+ Complex Complex::operator + (Complex c1)  
  {
  Complex temp;
  temp.real = real + c1.real;
  temp.img = img + c1.img;
  return temp;
  }
- Complex Complex::operator * (Complex c2)   // Overloading * Operator
+ Complex Complex::operator * (Complex c2)   
  {
  Complex tmp;
  tmp.real = real * c2.real - img * c2.img;
